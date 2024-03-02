edaa673391a5c16785d0c6d4cf97582f9e466392
Анастасия Серёгина <imaginejohn@rambler.ru>
2024-03-02 20:40:05 +0300

    routes testing added

---
+++ news/pytest_tests/conftest.py
@@ -0,0 +1,67 @@
+# conftest.py
+import pytest
+
+# Импортируем класс клиента.
+from django.test.client import Client
+
+# Импортируем модель заметки, чтобы создать экземпляр.
+from news.models import Comment, News
+
+@pytest.fixture
+# Используем встроенную фикстуру для модели пользователей django_user_model.
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
+
+
+@pytest.fixture
+def author_client(author):  # Вызываем фикстуру автора.
+    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
+    client = Client()
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
+    return client
+
+@pytest.fixture
+def news(author):
+    news = News.objects.create(  # Создаём объект заметки.
+        title='Заголовок',
+        text='Текст заметки',
+    )
+    return news
+
+@pytest.fixture
+def comment(news, author):
+    comment = Comment.objects.create(  # Создаём объект заметки.
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def form_data():
+    return {
+        'text': 'Текст комментария'
+    }
+
+@pytest.fixture
+# Фикстура запрашивает другую фикстуру создания заметки.
+def id_for_args(comment):
+    # И возвращает кортеж, который содержит slug заметки.
+    # На то, что это кортеж, указывает запятая в конце выражения.
+    return (comment.id,)
+
+
+
---
+++ news/pytest_tests/test_routes.py
@@ -0,0 +1,58 @@
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур
+    # в вызов функции pytest.lazy_fixture().
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_availability_for_comment_edit_and_delete(
+        parametrized_client, name, comment, expected_status
+):
+    url = reverse(name, args=(comment.id,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('news:edit', pytest.lazy_fixture('id_for_args')),
+        ('news:delete', pytest.lazy_fixture('id_for_args')),
+    ),
+)
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
+
+
